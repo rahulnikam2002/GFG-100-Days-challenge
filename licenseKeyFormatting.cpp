15942dc37f9206d8a56dda63a04c5af081e4a8f4
Rahul Nikam <codewithrahulnikam@gmail.com>
2022-10-01 18:33:53 +0530

    Day 2 - License Key Formatting challenge completed (21 Septembe 2022)

--- licenseKeyFormatting.cpp
+++ licenseKeyFormatting.cpp
@@ -1,12 +1,13 @@
-// Given a string S that consists of only alphanumeric characters and dashes. The string is separated into N + 1 groups by N dashes. Also given an integer K.
+// Given a string S that consists of only alphanumeric characters and dashes. The string is separated into N + 1 groups by N dashes. Also given an integer K. 
 
 // We want to reformat the string S, such that each group contains exactly K characters, except for the first group, which could be shorter than K but still must contain at least one character. Furthermore, there must be a dash inserted between two groups, and you should convert all lowercase letters to uppercase.
 
 // Return the reformatted string.
 
+
 // Example 1:
 
-// Input:
+// Input: 
 // S = "5F3Z-2e-9-w", K = 4
 // Output: "5F3Z-2E9W"
 // Explanation: The string S has been split into two
@@ -17,46 +18,40 @@
 // Input:
 // S = "2-5g-3-J", K = 2
 // Output: "2-5G-3J"
-// Explanation: The string s has been split
-// into three parts, each part has 2 characters
+// Explanation: The string s has been split 
+// into three parts, each part has 2 characters 
 // except the first part as it could
 // be shorter as mentioned above.
 
 // Code=>
 class Solution
 {
-public:
-    string ReFormatString(string S, int K)
-    {
+   public:
+    string ReFormatString(string S, int K){
+    	
+     string temp;
 
-        string temp;
+     int count = 0;
 
-        int count = 0;
+     for(int i=S.length()-1; i>=0; i--){
 
-        for (int i = S.length() - 1; i >= 0; i--)
-        {
+         if(S[i]=='-') continue;
+         if(S[i] >= 'a' and S[i] <= 'z'){
+            S[i] = toupper(S[i]);
+         }
+         temp.push_back(S[i]);
+         count++;
+         if(count == K){
+             count = 0;
+             temp.push_back('-');
+         }
+     }
+     if(temp[temp.size()-1] == '-'){
+        temp.pop_back();
+     }
 
-            if (S[i] == '-')
-                continue;
-            if (S[i] >= 'a' and S[i] <= 'z')
-            {
-                S[i] = toupper(S[i]);
-            }
-            temp.push_back(S[i]);
-            count++;
-            if (count == K)
-            {
-                count = 0;
-                temp.push_back('-');
-            }
-        }
-        if (temp[temp.size() - 1] == '-')
-        {
-            temp.pop_back();
-        }
-
-        reverse(temp.begin(), temp.end());
-        return temp;
+     (temp.begin(), temp.end());
+     return temp;
     }
 };
 
